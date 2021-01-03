fd6730742a44d5ca8ef321c59e375e35eaf5d314
Xearta <rsmcwow@gmail.com>
2021-01-03 13:47:34 -0500

    Used platform to create a ripple effect for android

--- components/MainButton.js
+++ components/MainButton.js
@@ -1,19 +1,40 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  TouchableNativeFeedback,
+  Platform,
+} from 'react-native';
 
 import Colors from '../constants/colors';
 
 const MainButton = props => {
+  // Creates a ripple effect for android (only supports android)
+  let ButtonComponent = TouchableOpacity;
+
+  if (Platform.OS === 'android' && Platform.Version >= 21) {
+    ButtonComponent = TouchableNativeFeedback;
+  }
+
   return (
-    <TouchableOpacity activeOpacity={0.6} onPress={props.onPress}>
-      <View style={styles.button}>
-        <Text style={styles.buttonText}>{props.children}</Text>
-      </View>
-    </TouchableOpacity>
+    <View style={styles.buttonContainer}>
+      <ButtonComponent activeOpacity={0.6} onPress={props.onPress}>
+        <View style={styles.button}>
+          <Text style={styles.buttonText}>{props.children}</Text>
+        </View>
+      </ButtonComponent>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
+  buttonContianer: {
+    borderRadius: 25,
+    overflow: 'hidden',
+  },
+
   button: {
     backgroundColor: Colors.primary,
     paddingVertical: 12,
