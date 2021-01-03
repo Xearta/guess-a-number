18ee701bbd3857ac3924904b0ade8ea232bd7e84
Xearta <rsmcwow@gmail.com>
2021-01-03 13:54:00 -0500

    Moved MainButton into two platform specific files for cleanup

--- components/MainButton.android.js
+++ components/MainButton.android.js
@@ -14,7 +14,7 @@ const MainButton = props => {
   // Creates a ripple effect for android (only supports android)
   let ButtonComponent = TouchableOpacity;
 
-  if (Platform.OS === 'android' && Platform.Version >= 21) {
+  if (Platform.Version >= 21) {
     ButtonComponent = TouchableNativeFeedback;
   }
 
